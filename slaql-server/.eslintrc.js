591c6c2da9e90a32fb9a1a87d9a85ac512ef32f1
kedmenecr <kedmenec.roberto@gmail.com>
2018-07-03 11:27:40 +0200

    Database finished.

--- slaql-server/.eslintrc.js
+++ slaql-server/.eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "airbnb-base"
+	"extends": "airbnb-base",
+	"rules": {
+		"indent": [2, "tab"],
+		"no-tabs": 0
+	}
+
+
 };
---
+++ slaql-server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import models from './models';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers,
+});
+
+const app = express();
+
+const graphqlEndpoint = '/graphql';
+
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+
+models.sequelize.sync().then(() => {
+	app.listen(8081);
+});
