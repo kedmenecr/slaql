f7d018b2e946822f2e8a0289d398ee83099a9231
kedmenecr <kedmenec.roberto@gmail.com>
2018-07-04 12:30:33 +0200

    Finished with react form for register, need to add verification and alert to user.

--- slaql-client/src/routes/Register.js
+++ slaql-client/src/routes/Register.js
@@ -1,60 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Input, Container, Header } from 'semantic-ui-react';
-import { gql, graphql } from 'react-apollo';
+import { gql, graphql } from 'react-apollo'
+
+
 
 class Register extends React.Component {
-  state = {
-    username: '',
-    email: '',
-    password: '',
-  };
+	state = {
+		username: '',
+		email: '',
+		password: ''
+	}
 
-  onSubmit = async () => {
-    const response = await this.props.mutate({
-      variables: this.state,
-    });
+	onChange = e => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	};
 
-    console.log(response);
-  };
+	onSubmit = () => {
 
-  onChange = e => {
-    const { name, value } = e.target;
-    // name = "email";
-    this.setState({ [name]: value });
-  };
+		const response = this.props.mutate({
+			variables: this.state
+		});
 
-  render() {
-    const { username, email, password } = this.state;
+		console.log(response);
+	}
 
-    return (
-      <Container text>
-        <Header as="h2">Register</Header>
-        <Input
-          name="username"
-          onChange={this.onChange}
-          value={username}
-          placeholder="Username"
-          fluid
-        />
-        <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
-        <Input
-          name="password"
-          onChange={this.onChange}
-          value={password}
-          type="password"
-          placeholder="Password"
-          fluid
-        />
-        <Button onClick={this.onSubmit}>Submit</Button>
-      </Container>
-    );
-  }
+	render() {
+
+		const { username, email, password } = this.state;
+
+		return (
+			<Container text>
+				<Header as='h2'>Register</Header>
+				<Input
+					name="username"
+					onChange={this.onChange}
+					value={username}
+					placeholder="Name"
+					fluid
+				/>
+				<Input
+					name="email"
+					onChange={this.onChange}
+					placeholder="Email"
+					fluid
+					value={email}
+				/>
+				<Input
+					name="password"
+					onChange={this.onChange}
+					type="password"
+					value={password}
+					placeholder="Password"
+					fluid />
+				<Button onClick={this.onSubmit}>
+
+				</Button>
+			</Container>
+		);
+	}
 }
 
 const registerMutation = gql`
-  mutation($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password)
-  }
+mutation($username: String!,$email: String!, $password: String!){
+  	register(username: $username,email: $email, password:$password)
+}
 `;
 
 export default graphql(registerMutation)(Register);
