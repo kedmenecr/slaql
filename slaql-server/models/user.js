593fca5cd914f32620d8eae57a6632f110373f20
kedmenecr <kedmenec.roberto@gmail.com>
2018-07-04 14:45:59 +0200

    Added Validation for register form.

--- slaql-server/models/user.js
+++ slaql-server/models/user.js
@@ -3,13 +3,30 @@ export default (sequelize, DataTypes) => {
     username: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'The username can only contain letters and numbers.',
+        },
+        len: {
+          args: [3, 25],
+          msg: 'The username can be between 3 and 25 characters long.',
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Invalid email',
+        },
+      },
     },
     password: DataTypes.STRING,
   });
+
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
